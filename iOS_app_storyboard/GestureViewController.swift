6c14a81e4d0d169e2cc931f04410fcae70389e23
Sean Lim <sealim@deloitte.com>
2020-03-18 08:59:48 +0800

    Added homepage and button. Replaced text to label

---
+++ iOS_app_storyboard/GestureViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  iOS_app_storyboard
+//
+//  Created by Lim, Sean Jericson Delas Alas on 17/03/2020.
+//  Copyright © 2020 deloitte. All rights reserved.
+//
+
+import UIKit
+
+class GestureViewController: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+       
+           img.isUserInteractionEnabled = true
+           
+           // SWIPE RIGHT
+           let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+           
+           swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+           
+           img.addGestureRecognizer(swipeRight)
+    
+           
+           
+           // SWIPE LEFT
+           let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+           
+           swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+           
+           img.addGestureRecognizer(swipeLeft)
+           
+           
+           
+           // SWIPE UP
+           let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+           
+           swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+           
+           img.addGestureRecognizer(swipeUp)
+           
+           
+           // SWIPE DOWN
+           let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+           
+           swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        
+           img.addGestureRecognizer(swipeDown)
+       }
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func swipeGesture(sendr: UISwipeGestureRecognizer?) {
+        label.textAlignment = .center
+        if let swipeGesture = sendr {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.right:
+                print("Right")
+                label.text = "RIGHT"
+            case UISwipeGestureRecognizer.Direction.left:
+                print("Left")
+                label.text = "LEFT"
+            case UISwipeGestureRecognizer.Direction.up:
+                print("Up")
+                label.text = "UP"
+            case UISwipeGestureRecognizer.Direction.down:
+                print("Down")
+                label.text = "DOWN"
+            default:
+                break
+            }
+        }
+    }
+}
+
--- iOS_app_storyboard/ViewController.swift
+++ iOS_app_storyboard/ViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  GesturesViewController.swift
 //  iOS_app_storyboard
 //
-//  Created by Lim, Sean Jericson Delas Alas on 17/03/2020.
+//  Created by Lim, Sean Jericson Delas Alas on 18/03/2020.
 //  Copyright © 2020 deloitte. All rights reserved.
 //
 
@@ -10,71 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var img: UIImageView!
-    
-    
-    @IBOutlet weak var text: UITextField!
-    
     override func viewDidLoad() {
-           super.viewDidLoad()
-       
-           img.isUserInteractionEnabled = true
-           
-           // SWIPE RIGHT
-           let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
-           
-           swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-           
-           img.addGestureRecognizer(swipeRight)
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
     
-           
-           
-           // SWIPE LEFT
-           let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
-           
-           swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-           
-           img.addGestureRecognizer(swipeLeft)
-           
-           
-           
-           // SWIPE UP
-           let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
-           
-           swipeUp.direction = UISwipeGestureRecognizer.Direction.up
-           
-           img.addGestureRecognizer(swipeUp)
-           
-           
-           // SWIPE DOWN
-           let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
-           
-           swipeDown.direction = UISwipeGestureRecognizer.Direction.down
-           
-           img.addGestureRecognizer(swipeDown)
-       }
 
+    /*
+    // MARK: - Navigation
 
-       @objc func swipeGesture(sendr: UISwipeGestureRecognizer?) {
-           
-           if let swipeGesture = sendr {
-               switch swipeGesture.direction {
-               case UISwipeGestureRecognizer.Direction.right:
-                   print("Right")
-                   text.text = "RIGHT"
-               case UISwipeGestureRecognizer.Direction.left:
-                   print("Left")
-                   text.text = "LEFT"
-               case UISwipeGestureRecognizer.Direction.up:
-                   print("Up")
-                   text.text = "UP"
-               case UISwipeGestureRecognizer.Direction.down:
-                   print("Down")
-                   text.text = "DOWN"
-               default:
-                   break
-               }
-           }
-       }
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-
